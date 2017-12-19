77489ccc5ded4c9db1bbeef0ae230a09164a23f4
Ferenc Gutierrez <ferenc.g87@gmail.com>
2017-12-19 08:33:34 -0600

    Server Side Logic and UI Touch up

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,97 @@
 module.exports = [{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }];
+        "name": "Ferenc Gutierrez",
+        "photo": "https://avatars2.githubusercontent.com/u/28332399?s=460&v=4",
+        "scores": [
+            1,
+            4,
+            2,
+            5,
+            2,
+            3,
+            3,
+            1,
+            1,
+            5
+        ]
+    },
+    {
+        "name": "Han Solo",
+        "photo": "https://images-eu.ssl-images-amazon.com/images/I/41JZBcE1O7L._SL500_AC_SS350_.jpg",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Luke Skywalker",
+        "photo": "https://www.fjackets.com/blog/wp-content/uploads/2016/09/Luke-Skywalker-Costume-A-New-Hope.jpg",
+        "scores": [
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5"
+        ]
+    },
+    {
+        "name": "Darth Vader",
+        "photo": "https://vignette.wikia.nocookie.net/starwars/images/3/37/Vader_TFU.jpg/revision/latest/scale-to-width-down/210?cb=20081111171655",
+        "scores": [
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Kylo Ren",
+        "photo": "https://cdn6.bigcommerce.com/s-2zs1uo/images/stencil/original/products/243/1110/kylorenmb270x270__27851.1505940854.jpg?c=2",
+        "scores": [
+            "5",
+            "1",
+            "5",
+            "3",
+            "4",
+            "2",
+            "4",
+            "3",
+            "5",
+            "3"
+        ]
+    },
+    {
+        "name": "Rey",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/a/af/Rey_Star_Wars.png",
+        "scores": [
+            "1",
+            "4",
+            "1",
+            "3",
+            "2",
+            "4",
+            "2",
+            "5",
+            "1",
+            "3"
+        ]
+    }
+];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,32 @@
 var path = require('path');
 var Friends = require('../data/friends.js');
 module.exports = function(server){
-    server.get('/api/friendslist', function(req, res){
+    server.get('/api/friends', function(req, res){
         res.json(Friends);
     });
 
-    server.post('/api/friendslist', function(req, res){
-        console.log(req.body);
-        var totalScore = 0;
-        req.body.scores.forEach(function(score){
-            totalScore += score;
+    server.post('/api/friends', function(req, res){
+        var closestMatch;
+        var oldDiff = 0;
+        
+        Friends.forEach(function(friend){
+            var diff = 0;
+            for(var i = 0; i < friend.scores.length; i++){
+                if(parseInt(req.body.scores[i]) != parseInt(friend.scores[i])){  
+                    diff += Math.abs(parseInt(req.body.scores[i]) + parseInt(friend.scores[i]));
+                }
+            }
+
+            if(diff < oldDiff){
+                closestMatch = friend;
+                oldDiff = diff;
+            } else if(oldDiff === 0){
+                closestMatch = friend;
+                oldDiff = diff;
+            }
         });
 
-
-
-        console.log("New User Total Score is " + totalScore);
         Friends.push(req.body);
-        res.send("You have been added");
+        res.send(closestMatch);
     });
 }
